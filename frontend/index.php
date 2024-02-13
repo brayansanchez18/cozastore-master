1ce05ceaad99e2de321756e27eb7fb1e4b2a7a36
brayan emanuel sanchez ramirez <brayan.sanchez.contacto@gmail.com>
2024-02-13 10:01:33 -0600

    se crean los modelos y controladores

--- frontend/index.php
+++ frontend/index.php
@@ -5,6 +5,11 @@
 /* -------------------------------------------------------------------------- */
 
 require_once 'controllers/plantilla.controlador.php';
+require_once 'controllers/productos.controlador.php';
+require_once 'controllers/slide.controlador.php';
+require_once 'controllers/usuarios.controlador.php';
+require_once 'controllers/carrito.controlador.php';
+require_once 'controllers/visitas.controlador.php';
 
 /* ------------------------------ CONTROLADORES ----------------------------- */
 
@@ -13,6 +18,11 @@ require_once 'controllers/plantilla.controlador.php';
 /* -------------------------------------------------------------------------- */
 
 require_once 'models/plantilla.modelo.php';
+require_once 'models/productos.modelo.php';
+require_once 'models/slide.modelo.php';
+require_once 'models/usuarios.modelo.php';
+require_once 'models/carrito.modelo.php';
+require_once 'models/visitas.modelo.php';
 
 /* --------------------------------- MODELOS -------------------------------- */
 
